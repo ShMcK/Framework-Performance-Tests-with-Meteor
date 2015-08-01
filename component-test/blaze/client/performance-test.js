15bae8af4c276e988275152317251c912bc2a07c
ShMcK <me@shmck.com>
2015-08-01 22:30:15 +0900

    blaze component test

---
+++ component-test/blaze/client/performance-test.js
@@ -0,0 +1,42 @@
+Session.setDefault('count', 0);
+Session.setDefault('runH', false); // html list
+Session.setDefault('runT', false); // template list
+
+function arrayify(list, n) {
+  if (Session.get(list)) {
+    var array = [];
+    for (var i = 0; i < n; i++) {
+      array.push(i);
+    }
+    return array;
+  } else {
+    return [];
+  }
+}
+
+Template.performanceTest.helpers({
+  'counts': function () {
+    return [10, 100, 500, 1000, 2000, 3000, 4000, 5000];
+  },
+  'hListArray': function () {
+    return arrayify('runH', parseInt(Session.get('count')));
+  },
+  'tListArray': function () {
+    return arrayify('runT', parseInt(Session.get('count')));
+  }
+});
+
+Template.performanceTest.events({
+  'click .count-selector': function (e) {
+    var value = $(e.currentTarget).val();
+    Session.set('count', value);
+  },
+  'click #reset': function () {
+    Session.set('runH', false);
+    Session.set('runT', false);
+  },
+  'click #run': function (e) {
+    var letter = $(e.currentTarget).val();
+    Session.set('run' + letter, true);
+  }
+});
